bbdc3e3e5b762fe3b0f174182df9890f1c61df0d
jujin <jujin1324@daum.net>
2019-05-02 14:58:40 +0900

    EmailServiceImpl.java : helper.setFrom(fromAddr); 추가

--- src/main/java/com/spring/mail/sender/demo/service/EmailService.java
+++ src/main/java/com/spring/mail/sender/demo/service/EmailService.java
@@ -2,6 +2,8 @@ package com.spring.mail.sender.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 /**
  * Created by Yoo Ju Jin(yjj@hanuritien.com)
  * Created Date : 2019-04-29
@@ -10,4 +12,6 @@ import org.springframework.stereotype.Service;
 public interface EmailService {
     void sendSimpleMessage(String to, String subject, String text);
     void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment);
+    void sendMessageWithAttachment(String to, String subject, String text, File file);
+
 }
--- src/main/java/com/spring/mail/sender/demo/service/EmailServiceImpl.java
+++ src/main/java/com/spring/mail/sender/demo/service/EmailServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.mail.sender.demo.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
@@ -24,9 +25,13 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     public JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromAddr;
+
     @Override
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromAddr);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
@@ -38,13 +43,30 @@ public class EmailServiceImpl implements EmailService {
         MimeMessage message = emailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(fromAddr);
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(text);
             FileSystemResource file = new FileSystemResource(pathToAttachment);
             helper.addAttachment("Enter attachment file name", file);
         } catch (MessagingException me) {
-            log.debug("[jujin] {}", me);
+            log.error("error - {}", me.getCause());
+        }
+        emailSender.send(message);
+    }
+
+    @Override
+    public void sendMessageWithAttachment(String to, String subject, String text, File file) {
+        MimeMessage message = emailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(fromAddr);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+            helper.addAttachment(file.getName(), file);
+        } catch (MessagingException me) {
+            log.error("error - {}", me.getCause());
         }
         emailSender.send(message);
     }
